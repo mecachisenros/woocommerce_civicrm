0671cba959eae35e624d6fff865dc5e5e5718dd2
Sabrina Leroy <sabrina@avecnous.eu>
2018-08-09 13:45:41 +0200

    fix random user saved in civicrm when manual order 5

--- includes/class-woocommerce-civicrm-manager.php
+++ includes/class-woocommerce-civicrm-manager.php
@@ -565,8 +565,7 @@ class Woocommerce_CiviCRM_Manager {
 		 * @param array $params The params to be passsed to the API
 		 */
 			$contribution = civicrm_api3( 'Contribution', 'create', apply_filters( 'woocommerce_civicrm_contribution_create_params', $params ) );
-			var_dump($contribution);
-			die();
+	
 			if(isset($contribution['id']) && $contribution['id']){
 				// Adds order note in reference to the created contribution
 				$order->add_order_note(sprintf(__('Contribution %s has been created in CiviCRM', 'woocommerce-civicrm'),

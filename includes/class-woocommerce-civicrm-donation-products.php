5d868f5453c2df6c9824bb4aea66319b4aa7d88f
Sabrina Leroy <sabrina@avecnous.eu>
2018-08-14 20:01:08 +0200

    Shordcode created, displays the list of prices depending on the product ID, displays the tax return, dynamically with JS, Form, gateways and CSS to do

--- includes/class-woocommerce-civicrm-donation-products.php
+++ includes/class-woocommerce-civicrm-donation-products.php
@@ -25,12 +25,8 @@ class Woocommerce_CiviCRM_Donation_Products {
         add_action( 'woocommerce_process_product_meta', array(&$this, 'product_save') );
 
 				add_action( 'admin_enqueue_scripts', array(&$this, 'donation_admin_script') );
-			//	add_action( 'bulk_edit_custom_box', array(&$this, 'add_contribution_to_quick_edit'), 10, 2 );
 
-
-				// Bulk / quick edit.
-			//	add_action( 'save_post', array( $this, 'bulk_and_quick_edit_hook' ), 10, 2 );
-			//	add_action( 'contributions_product_bulk_and_quick_edit', array( $this, 'bulk_and_quick_edit_save_post' ), 10, 2 );
+				add_shortcode( 'woocommerce-civicrm-donation', array(&$this, 'woocommerce_civicrm_donation') );
 
 
     }
@@ -44,106 +40,150 @@ class Woocommerce_CiviCRM_Donation_Products {
         //global $woocommerce, $post;
         echo '<div class="options_group donation_fields">';
 
-					$args = array(
-					'label' => 'Price list choices', // Text in Label
+					$arg_suggestion = array(
+					'label' => 'One-time Price list choices', // Text in Label
 					'placeholder' => '25,45,55*,100',
-					'id' => '_civicrm_donation_price_list', // required
-					'name' => 'civicrm_donation_price_list', //name will set from id if empty
+					'id' => '_civicrm_donation_onetime_price_list', // required
+					'name' => 'civicrm_donation_onetime_price_list', //name will set from id if empty
 
-					'description' => 'List here the choices you want to see on the Donation page. Separate by "," and give * to the default one.'
+					'description' => 'List here the choices you want to see on the Donation page for the One-Time Donation. Separate by "," and give * to the default one.'
 					);
-					woocommerce_wp_text_input( $args );
+					woocommerce_wp_text_input( $arg_suggestion );
+
+					$arg_suggestion = array(
+					'label' => 'Recurring Price list choices', // Text in Label
+					'placeholder' => '5,10,15,30',
+					'id' => '_civicrm_donation_recurring_price_list', // required
+					'name' => 'civicrm_donation_recurring_price_list', //name will set from id if empty
+
+					'description' => 'List here the choices you want to see on the Donation page for the Recurring Donation. Separate by ",".'
+					);
+					woocommerce_wp_text_input( $arg_suggestion );
+
+
+					$arg_tax_return = array(
+					'label' => 'Percentage of Tax Return (%)', // Text in Label
+					'placeholder' => '66',
+					'id' => '_civicrm_donation_tax_return', // required
+					'name' => 'civicrm_donation_tax_return', //name will set from id if empty
+
+					'description' => 'Enter here the percentage of tax return the benefactor will get back after donation.'
+					);
+					woocommerce_wp_text_input( $arg_tax_return );
         echo '</div>';
 
     }
 
-/*
-		// Contribution fields for products
-    function contribution_fields_bulk() {
 
-        //global $woocommerce, $post;
-        echo '
-								<div class="inline-edit-group">
-								<label class="alignleft">
-									<span class="title">'.__( 'Contribution type', 'woocommerce-civicrm' ).'</span>
-									<span class="input-text-wrap">
-
-
-
-
-
-
-				<select style="" id="_civicrm_contribution_type" name="civicrm_contribution_type" class="select short">';
-
-				$contributions_types = WCI()->helper->financial_types;
-        $options =
-            array(
-
-							''  => __( '— No change —', 'woocommerce-civicrm' ),
-
-            ) +
-            $contributions_types +
-            array(
-                'exclude' => '-- '.__('Exclude', 'woocommerce-civicrm')
-            );
-
-				foreach ( $options as $key => $value ) {
-						echo '<option value="' . esc_attr( $key ) . '">' . $value . '</option>';
-				}
-        echo '	</select>
-				</span>
-			</label>
-		</div>';
-	}*/
 
     //Save
     function product_save( $post_id ){
-        $civicrm_donation_price_list = sanitize_text_field($_POST['civicrm_donation_price_list']);
-        update_post_meta( $post_id, '_civicrm_donation_price_list', $civicrm_donation_price_list );
+        $civicrm_donation_tax_return = sanitize_text_field($_POST['civicrm_donation_tax_return']);
+        update_post_meta( $post_id, '_civicrm_donation_tax_return', $civicrm_donation_tax_return );
+
+
+		    $civicrm_donation_onetime_price_list = sanitize_text_field($_POST['civicrm_donation_onetime_price_list']);
+		    update_post_meta( $post_id, '_civicrm_donation_onetime_price_list', $civicrm_donation_onetime_price_list );
+
+				$civicrm_donation_recurring_price_list = sanitize_text_field($_POST['civicrm_donation_recurring_price_list']);
+		    update_post_meta( $post_id, '_civicrm_donation_recurring_price_list', $civicrm_donation_recurring_price_list );
     }
-/*
-		function add_contribution_to_quick_edit( $column_name, $post_type  ){
-				if ( 'product_cat' !== $column_name || 'product' !== $post_type ) {
-					return;
+
+		function woocommerce_civicrm_donation( $atts ){
+			$result = array(
+				"success" => "true",
+				"output" => "",
+				"error" => ""
+			);
+
+			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+			if(is_plugin_active('woocommerce/woocommerce.php') && is_plugin_active('woocommerce-name-your-price/woocommerce-name-your-price.php')){
+				wp_enqueue_script( 'donation-front-script', WOOCOMMERCE_CIVICRM_URL . 'js/donation-front.js', array(), '1.0' );
+				wp_enqueue_style('donation-style', WOOCOMMERCE_CIVICRM_URL . 'css/donation.css' );
+				$result["output"] .= '
+				<div class="woocommerce_civicrm_donation">
+						<div class="col_wrapper">
+						<div class="group_choices">';
+
+				if(isset($atts['product-id'])&&$atts['product-id']!=""&&$atts['product-id']!=="0"){
+
+					$product = wc_get_product( $atts['product-id'] );
+					if($product===FALSE || $product===NULL){
+						$result["error"] .= __('The product ID provided in the attribute product-id is not a valid product ID.','woocommerce-civicrm').'<br/>';
+						$result["success"]= false;
+					}else{
+						$_nyp = get_post_meta($product->get_id() , "_nyp" , true );
+						if($_nyp == "yes"){
+							$result["output"] .= '
+							<div class="col_wrapper">';
+							if(metadata_exists('post' ,$product->get_id() , "_civicrm_donation_onetime_price_list" )){
+								$_civicrm_donation_onetime_price_array = explode( ",",get_post_meta($product->get_id() , "_civicrm_donation_onetime_price_list" , true ));
+								if(is_array($_civicrm_donation_onetime_price_array) && count($_civicrm_donation_onetime_price_array)>0){
+									$result["output"] .= '<div class="onetime_choices"><h2>'.__('I donate once','woocommerce-civicrm').'</h2>';
+									foreach ($_civicrm_donation_onetime_price_array as $_civicrm_donation_price) {
+										$checked = "";
+										if(strpos($_civicrm_donation_price ,"*")){
+											$checked = ' checked="checked" ';
+											$_civicrm_donation_price = str_replace("*" , "", $_civicrm_donation_price);
+										}
+										$result["output"] .= '<label><input type="radio" value="'.$_civicrm_donation_price.'" name="price" '.$checked.'/><span> '.$_civicrm_donation_price.' '.__('€','woocommerce-civicrm').'</span></label>';
+									}
+									$result["output"] .= '<label>	<input type="radio" value="other_price_onetime" name="price"/>
+																								<span>'.__('Another amount','woocommerce-civicrm').' <input type="text" name="other_price_onetime" id="other_price_onetime" class="other_price" disabled> '.__('€','woocommerce-civicrm').'</span></label>';
+									$result["output"] .= '</div>';
+								}
+							}
+
+							if(metadata_exists('post' ,$product->get_id() , "_civicrm_donation_recurring_price_list" )){
+								$_civicrm_donation_recurring_price_array = explode( ",",get_post_meta($product->get_id() , "_civicrm_donation_recurring_price_list" , true ));
+								if(is_array($_civicrm_donation_recurring_price_array) && count($_civicrm_donation_recurring_price_array)>0){
+									$result["output"] .= '<div class="recurring_choices wcd_col"><h2>'.__('I donate monthly','woocommerce-civicrm').'</h2>';
+									foreach ($_civicrm_donation_recurring_price_array as $_civicrm_donation_price) {
+										$result["output"] .= '<label><input type="radio" value="'.$_civicrm_donation_price.'" name="price" class="recurring_choices"/><span> '.$_civicrm_donation_price.' '.__('€','woocommerce-civicrm').'</span></label>';
+									}
+									$result["output"] .= '<label>	<input type="radio" value="other_price_recurring" class="recurring_choices" name="price"/>
+																								<span>'.__('Another amount','woocommerce-civicrm').' <input type="text" name="other_price_recurring" id="other_price_recurring" class="other_price" disabled> '.__('€','woocommerce-civicrm').'</span></label>';
+									$result["output"] .= '</div>';
+								}
+							}
+
+							$result["output"] .= '	</div>';
+							$result["output"] .= '<input type="text" value="false" id="is_recurring" class="is_recurring">';
+							$result["output"] .= '<input type="text" value="" id="actual_price" class="actual_price">';
+							if(metadata_exists('post' ,$product->get_id() , "_civicrm_donation_tax_return" )){
+								$_civicrm_donation_tax_return = get_post_meta($product->get_id() , "_civicrm_donation_tax_return", true);
+
+								$result["output"] .= '<input type="text" value="'.$_civicrm_donation_tax_return.'" id="tax_return" class="tax_return">';
+								$result["output"] .= '<div class="tax_return">
+
+								<p>'. sprintf(__('Thanks to the tax return of %s','woocommerce-civicrm'), $_civicrm_donation_tax_return).' %, <br/>
+								my <span id="actual_price_calculation"></span> '.__('€','woocommerce-civicrm').' donation will only cost me <span id="after_tax_price_calculation"></span> '.__('€','woocommerce-civicrm').'.
+								</p></div>';
+							}
+						}else{
+							$result["error"] .= __('The Donation Product you have given is not a Name Your Price Product. Please, edit the product and check the Name Your Price checkbox.','woocommerce-civicrm').'<br/>';
+							$result["success"]= false;
+						}
+					}
+				}else{
+					$result["error"] .= __('You need to fill at least the Product ID for the Donation Product you want to show. The attribute is product-id.','woocommerce-civicrm').'<br/>';
+					$result["success"]= false;
 				}
-				echo '<div class="inline-edit-col-right" style="float:right;">';
-				$this->contribution_fields_bulk();
-				echo '</div>';
+				$result["output"] .= '	<div class="gateways">gateways</div>';
+				$result["output"] .= '	</div>';
+				$result["output"] .= '	<div class="form_basket">form</div>';
+
+				$result["output"] .= '</div>';
+			}else{
+				$result["error"] .= __('This Shortcode needs WooCommerce and WooCommerce Name Your Price to be activated to work.','woocommerce-civicrm') .'<br/>';
+				$result["success"]= false;
+			}
+			if($result["success"]){
+				return $result["output"];
+			}else{
+				return $result["error"];
+			}
 		}
-
-
-*/
-
-		/**
-		 * Offers a way to hook into save post without causing an infinite loop
-		 * when quick/bulk saving product info.
-		 *
-		 * @since 3.0.0
-		 * @param int    $post_id Post ID being saved.
-		 * @param object $post Post object being saved.
-
-		 function bulk_and_quick_edit_hook( $post_id, $post ) {
-			remove_action( 'save_post', array( $this, 'bulk_and_quick_edit_hook' ) );
-			do_action( 'contributions_product_bulk_and_quick_edit', $post_id, $post );
-			add_action( 'save_post', array( $this, 'bulk_and_quick_edit_hook' ), 10, 2 );
-		}
-		/**
-	 * Quick and bulk edit saving.
-	 *
-	 * @param int    $post_id Post ID being saved.
-	 * @param object $post Post object being saved.
-	 * @return int
-
-	 function bulk_and_quick_edit_save_post( $post_id, $post ) {
-		 if(isset($_GET['civicrm_contribution_type'])){
-
-			 $civicrm_contribution_type = sanitize_text_field($_GET['civicrm_contribution_type']);
-				update_post_meta( $post_id, '_civicrm_contribution_type', $civicrm_contribution_type );
-		 }
-
-	 }
-*/
-
 }
 
 
